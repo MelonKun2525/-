93b1939421ec9b8e0eb10f9b9252376d54e679c1
めろんくん <melonkun0531@gmail.com>
2019-09-20 00:24:51 +0900

    Upload

---
+++ ポインタ1/po1.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+void main(void){
+
+  int get, d, a[10] = {22, 60, 5, 87, 65, 0, 56, 12, 3, 76}, *p;
+
+  p = a;
+
+  printf("1～10の数値を入力=>");
+  scanf("%d", &get);
+
+  p = p + (get - 1);
+  d = *p;
+
+  printf("dの値は、%d です。\n", d);
+  printf("終了します。\n");
+
+}
---
+++ ポインタ1/po2.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+
+void main(void){
+
+  int get, d, a[10] = {22, 60, 5, 87, 65, 0, 56, 12, 3, 76}, *p;
+
+  p = a;
+
+  while(1){
+
+    do{
+
+      printf("1～10の数値を入力=>");
+      scanf("%d", &get);
+
+    }while(get < 1 || get > 10);
+
+    p = p + (get - 1);
+    d = *p;
+
+    printf("dの値は、%d です。\n", d);
+
+    p = p - (get - 1);
+
+  }
+}
