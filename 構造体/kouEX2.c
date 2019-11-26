125d045611ad73cacb1026a9bad440d23ba419e3
めろんくん <49083676+MelonKunGX@users.noreply.github.com>
2019-11-27 00:41:01 +0900

    アップロード

---
+++ 構造体/kouEX2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+struct pokemon{
+
+  int no;
+  char name[20];
+  int hp;
+  int atk;
+  int def;
+
+};
+
+int main(void){
+
+  struct pokemon nakama[4] = {
+    {1, "ピカチュウ", 100, 67, 60},
+    {2, "ルギア", 250, 120, 101},
+    {3, "エルフーン", 89, 60, 50},
+    {4, "ホエルオー", 358, 123, 45}
+  }, *p;
+  int in;
+
+  p = nakama;
+
+  printf("番号を入力=>");
+  scanf("%d", &in);
+
+  if(in >= 1 && in <= 4){
+
+    printf("名前:%s ", (p + in - 1)->name);
+    printf("HP:%d ", (p + in - 1)->hp);
+    printf("ATK:%d ", (p + in - 1)->atk);
+    printf("DEF:%d\n", (p + in - 1)->def);
+
+  }else{
+
+    printf("その番号は存在しません。\n");
+
+  }
+
+  printf("終了します。\n");
+  return 0;
+
+}
