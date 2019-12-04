fc3f80bafa0d9476a22b21475d7ee2a8858c64ae
めろんくん <49083676+MelonKunGX@users.noreply.github.com>
2019-12-04 23:35:40 +0900

    アップデート

--- 期末課題(上級者向け)/rk2.c
+++ 期末課題(上級者向け)/rk2.c
@@ -4,14 +4,14 @@ int main(void){
 
   int in, re = -999;
 
-  do{
+  while(in != 999){
+
+    re += in;
 
     printf("数値入力=>");
     scanf("%d", &in);
 
-    re += in;
-
-  }while(in != 999);
+  }
 
   printf("合計は%dです。\n", re);
 
