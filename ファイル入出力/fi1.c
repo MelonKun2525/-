b74a81d222751db1d2af3c17901fe51e96805817
めろんくん <melonkun0531@gmail.com>
2020-01-17 01:53:10 +0900

    fi1.cを追加

---
+++ ファイル入出力/fi1.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+  float sin, tai;
+  char name[20];
+  FILE *fp;
+
+  if((fp = fopen("kd1.txt", "w")) == NULL){
+
+    printf("ファイルがオープンできません。\n");
+    exit(1);
+
+  }
+
+  printf("名前　　身長　体重\n");
+
+  while(scanf("%s %f %f", name, &sin, &tai) != EOF)
+    fprintf(fp, "%s %.1f %.1f", name, sin, tai);
+
+  fclose(fp);
+  printf("終了します。\n");
+  return 0;
+
+}
